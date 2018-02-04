9bdd278da3b6b5227c653e64f68aaa143ba5d540
Filip Stangricki <stangricki@gmail.com>
2018-02-04 20:34:04 +0100

    Initial commit

---
+++ country-app/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './store';
+
+render(
+    <Provider store={store}>
+        <h1>Inicjalizacja projektu</h1>
+    </Provider>,
+    document.getElementById('root')
+);
+
